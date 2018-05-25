a90cabb097d61019424fd0bcec6ffdaff85a60c8
ite102770 <ite102770@383cc300-1c0e-49e2-a6cb-a3da9cc689f7>
2018-05-25 22:51:09 +0000

    working on element class

--- ueb03/grp22_ueb03/src/grp22_ueb03/MyList.java
+++ ueb03/grp22_ueb03/src/grp22_ueb03/MyList.java
@@ -10,5 +10,29 @@ package grp22_ueb03;
  * @author Alireza
  */
 public class MyList {
+    private Element element;
     
+    public boolean isEmpty(){
+        return element == null;
+    }
+    public void appendElement(char value){
+        if(isEmpty()){
+            element = new Element();
+            element.setValue(value);
+        }
+        else{
+           element = element.appendElement(value);
+        }
+    }
+    public void insertElementSorted(char value){
+        if(isEmpty()){
+            element = new Element();
+            element.setValue(value);
+        }
+        else{
+                if (element.existsElement(value)){
+                    element = element.insertElementSorted(value);
+                }
+        }
+    }
 }
