f2da999653d2739a6807bc4322278e16307d0976
ite102770 <ite102770@383cc300-1c0e-49e2-a6cb-a3da9cc689f7>
2018-05-25 20:08:23 +0000

    finalized with prints

--- grp22_ueb03/src/grp22_ueb03/Element.java
+++ grp22_ueb03/src/grp22_ueb03/Element.java
@@ -10,13 +10,13 @@ package grp22_ueb03;
  * @author Alireza
  */
 public class Element {
-    private int value;
+    private char value;
     private Element next;
     
-    public int getValue(){
+    public char getValue(){
         return this.value;
     }
-    public void setValue(int value){
+    public void setValue(char value){
         this.value = value;
     }
     public Element getElement(){
@@ -25,9 +25,58 @@ public class Element {
     public void setNext(Element next){
         this.next = next;
     }
-    public Element appendElement(int value){
-        Element newElement = new Element();
-        this.next = newElement;
+    public Element getNext(){
+        return this.next = next;
+    }
+    public Element appendElement(char value) {
+        if (this.next == null) {
+            Element newElement = new Element();
+            newElement.setValue(value);
+            this.next = newElement;
+        } else {
+            this.next = this.next.appendElement(value);
+        }
         return this;
+    }
+    public Element deleteElement(char value) {
+
+        if (this.value == value) {
+            return this.next;
+        } else {
+            if (this.next != null) {
+                this.next = this.next.deleteElement(value);
+            }
+            return this;
+        }
+    }
+    public Element insertElementSorted(char value) {
+        if (this.value > value) {
+            Element newElement = new Element();
+            newElement.setValue(value);
+            newElement.setNext(next);
+            return this;
+        } else if (this.next == null) {
+            Element newElement = new Element();
+            newElement.setValue(value);
+            this.next = newElement;
+            return this;
+        } else {
+            this.next = this.next.insertElementSorted(value);
+            return this;
+        }
+    }
+    public int size(){
+        int size = 0;
+        if(this.value == null){
+            size = 0;  
+        }
+        else if(this.next == null){
+            size = 1;
+        }
+        else{
+            this.next = this.next.size();
+            size++;
+        }
+        return size;
     }
 }
